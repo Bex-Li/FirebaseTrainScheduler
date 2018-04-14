e3f3d4406d385b340a6b852e14700aa75bec8f1e
Bex-Li <rebekkikko@gmail.com>
2018-04-14 10:06:09 -0700

    updates to js and html files

---
+++ assets/js/train.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    console.log("ready!");
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyANhsTqtXCqhrL8ddYAF4ub8SymJRTsgRg",
+        authDomain: "week7firebaseproject.firebaseapp.com",
+        databaseURL: "https://week7firebaseproject.firebaseio.com",
+        projectId: "week7firebaseproject",
+        storageBucket: "week7firebaseproject.appspot.com",
+        messagingSenderId: "332302097897"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+
+    var trainName = 0;
+    var trainDestination = 0;
+
+
+
+
+
+
+
+});
