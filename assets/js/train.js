6f0749c4ef4c299707ab050e58aaf43ea2eaa34a
Bex-Li <rebekkikko@gmail.com>
2018-04-17 00:32:01 -0700

    updated html  and js files

--- assets/js/train.js
+++ assets/js/train.js
@@ -14,8 +14,27 @@ $(document).ready(function () {
 
     var trainName = 0;
     var trainDestination = 0;
+    var firstTrainTime = 0;
+    var frequency = 0;
 
 
+// click listner on submit button
+$( "#submit" ).on( "click", function(event) {
+    event.preventDefault();
+    console.log("test");
+    var trainName = $("#trainName").val().trim();
+    console.log(trainName);
+    var destination = $("#destination").val().trim();
+    console.log(destination);
+    var firstTrainTime = $("#firstTrainTime").val().trim();
+    console.log(firstTrainTime);
+    var frequency = $("#frequency").val().trim();
+    console.log(frequency);
+  });
+
+// when you click submit we retrieve user entries
+// capture data in click listener via the vars
+// use vars to update firebase db
 
 
 
